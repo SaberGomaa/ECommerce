5ccf7afa24e6bcd01628175ddbcbf384d1d39463
sabegomaa261 <sabergomaa261@gmail.com>
2022-09-20 21:34:19 +0200

    Products

---
+++ Controllers/productController.cs
@@ -0,0 +1,22 @@
+﻿using ECommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Controllers
+{
+    public class productController : Controller
+    {
+        myDbContext context = new myDbContext();
+        public IActionResult Index()
+        {
+            return RedirectToAction("view", "product");
+        }
+        
+        public IActionResult show()
+        {
+            List<Product> products = context.products.OrderBy(e=>e.Price).ToList();
+
+            return View(products);
+        }
+
+    }
+}
--- Models/Product.cs
+++ Models/Product.cs
@@ -6,11 +6,18 @@ namespace ECommerce.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required(ErrorMessage = "*")]
+
         public string Name { get; set; }
+        [Required(ErrorMessage = "*")]
         public double Price { get; set; }
+        [Required(ErrorMessage = "*")]
         public string  Img { get; set; }
+
         public string Color { get; set; } // drop downlist Options
         public string Size { get; set; } // drop downlist Options
+       
+        [Required(ErrorMessage ="*")]
         public int Quantity { get; set; }
         public string? Details { get; set; }
 
