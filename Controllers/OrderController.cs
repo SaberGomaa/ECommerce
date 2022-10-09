879da5273891b1cdc2e05822a6a7185063bc8836
sabegomaa261 <sabergomaa261@gmail.com>
2022-10-09 20:08:52 +0200

    order

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -5,15 +5,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce.Controllers
 {
-
-
     public class OrderController : Controller
     {  
       
         public myDbContext context = new myDbContext();
         public IActionResult Index()
         {
-
             return View();
         }
 
